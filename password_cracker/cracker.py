c1a4a01f0be2d002c375abaaecc5bb6f62d70622
Travis Smith <travissmith94@hotmail.com>
2016-03-08 23:26:08 -0500

    Creates a rainbow table

--- password_cracker/cracker.py
+++ password_cracker/cracker.py
@@ -4,6 +4,7 @@ from sys import argv, exit
 import hashlib
 
 HASH_FILE_NAME = 'hashes.txt'
+RAINBOW_TABLE_DELIMETER = 'COOL_UWI_DELIMETER'
 
 def add_to_hash(word_list):
     hash_file = open(HASH_FILE_NAME , 'a')
@@ -12,8 +13,9 @@ def add_to_hash(word_list):
     total = len(word_list)
 
     for word in word_list:
-        hashed_word = hashlib.sha1(word.encode()).hexdigest()
-        hash_file.write(hashed_word)
+        sha1_hash = hashlib.sha1(word.encode()).hexdigest()
+        md5_hash = hashlib.md5(word.encode()).hexdigest()
+        hash_file.write(sha1_hash + RAINBOW_TABLE_DELIMETER + md5_hash)
         hash_file.write('\n')
         if(count % 10000 == 0):
             print '%d of %d words hashed' % (count, total)
@@ -29,10 +31,15 @@ def crack(word_list):
     encrypted_passwords = hash_file.read().split('\n')
 
     for word in word_list:
-        hashed_word = hashlib.sha1(word.encode()).hexdigest()
-        if(hashed_word in encrypted_passwords):
-            print 'Match found: %r' % word
-            matches += 1
+        sha1_hash = hashlib.sha1(word.encode()).hexdigest()
+        md5_hash  = hashlib.md5(word.encode()).hexdigest()
+        for encrypted_password in encrypted_passwords:
+            columns = encrypted_password.split(RAINBOW_TABLE_DELIMETER)
+            if len(columns) > 1 and (sha1_hash == columns[0] or md5_hash == columns[1]):
+                print 'Match found: %r' % word
+                matches += 1
+                break
+
     if matches == 0:
         print 'No matches found'
     else:
