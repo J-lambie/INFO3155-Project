23a602575fee3593d223fed092ae08e5e026ca03
Travis Smith <travissmith94@hotmail.com>
2016-03-16 17:00:02 -0500

    Template

---
+++ worm/front-end/cool_program.py
@@ -0,0 +1,18 @@
+def send_email(name , email):
+    return None
+
+def draw_tree():
+    return None
+
+if __name__ == '__main__':
+    #Do Stuff To make Tree or whatever
+    print """
+    Like what you see?
+    Share with your friends:"""
+
+    name = raw_input('Enter Friends Name here: ')
+    email= raw_input('Enter Friends Email address: ')
+
+    #Send Email Here
+
+    #Write Junk to Hard Drive here
