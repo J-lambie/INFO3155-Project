43e798e02df1c714a977a65fed6217c888c7fc33
J-lambie <kimmy_jill@hotmail.com>
2016-03-16 21:35:01 -0500

    added a tree thingie

--- worm/front-end/cool_program.py
+++ worm/front-end/cool_program.py
@@ -1,11 +1,23 @@
+import os
 def send_email(name , email):
     return None
 
 def draw_tree():
-    return None
+	print""""
+        __  __
+      _/_ \/ _\_
+     O/_ \||/ _\O
+     O/ \ \/ / \O                _  _
+      O   ||   O                  \/ 
+          ||                      ||
+          ||                      ||
+.)(.....||||||........))(O.......||||...
+"""
+   
 
 if __name__ == '__main__':
     #Do Stuff To make Tree or whatever
+    draw_tree()
     print """
     Like what you see?
     Share with your friends:"""
@@ -16,3 +28,6 @@ if __name__ == '__main__':
     #Send Email Here
 
     #Write Junk to Hard Drive here
+
+
+
